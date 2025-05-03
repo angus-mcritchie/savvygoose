baba50597c64edc0717d2c8a60f5de71c8408e5e
Angus McRitchie <angus@hobbiesdirect.com.au>
2025-05-03 20:31:01 +1000

    added print to barcode

--- resources/js/data/barcode.js
+++ resources/js/data/barcode.js
@@ -1,13 +1,61 @@
 import Code128Generator from "code-128-encoder";
 import "../../css/barcode-generator.css";
+import Printd from "printd";
 
 export default () => ({
     label: null,
     value: null,
     code: null,
+    print: null,
+    url: window.location.href,
 
     init() {
         this.$watch('value', () => this.code = (new Code128Generator()).encode(this.value));
+
+        this.initFromUrl();
+
+        this.$watch('label', () => this.updateUrl());
+        this.$watch('value', () => this.updateUrl());
+        this.$watch('print', () => this.updateUrl());
+
+        this.updateUrl();
+    },
+
+    initFromUrl() {
+        const params = new URLSearchParams(window.location.search);
+
+        if (params.has('label')) {
+            this.label = params.get('label');
+        }
+
+        if (params.has('value')) {
+            this.value = params.get('value');
+        }
+        if (params.has('print')) {
+            this.print = params.get('print') === 'true';
+        }
+
+        if (this.print) {
+            this.$nextTick(() => this.printBarcode());
+        }
+    },
+
+    updateUrl() {
+        const params = new URLSearchParams(window.location.search);
+
+        if (this.label) {
+            params.set('label', this.label);
+        }
+
+        if (this.value) {
+            params.set('value', this.value);
+        }
+
+        if (this.print) {
+            params.set('print', true);
+        }
+
+        this.url = `${window.location.origin}${window.location.pathname}${params.toString() ? '?' + params.toString() : ''}`;
     },
 
     getLabel() {
@@ -20,5 +68,9 @@ export default () => ({
 
     getValue() {
         return this.value || 'value';
+    },
+
+    printBarcode() {
+        (new Printd()).print(this.$refs.barcodeCanvas, [this.$refs.stylesheet.href]);
     }
 })
--- resources/views/barcode-generator.blade.php
+++ resources/views/barcode-generator.blade.php
@@ -3,48 +3,57 @@
         Barcode Generator
     </h1>
 
-    <link rel="stylesheet" href="{{ asset('assets/css/barcode-generator.css') }}">
 
-
-    <div class="mx-auto max-w-[1200px]" x-data="barcode">
+    <form class="mx-auto max-w-[1200px]" x-data="barcode" x-on:submit.prevent="printBarcode">
+        <link href="{{ Vite::asset('resources/css/barcode-generator.css') }}" rel="stylesheet" x-ref="stylesheet">
         <div class="grid gap-6 lg:grid-cols-2">
             <div class="rounded-lg border border-black/10 p-8 dark:border-white/10">
                 <flux:heading class="mb-6 border-b border-black/10 pb-4 dark:border-white/10" size="xl">1. Generate</flux:heading>
-                <div class="grid gap-4">
-                    <flux:input x-model="label" label="Label" name="label" placeholder="my label" />
-                    <flux:input x-model="value" label="Value" name="value" placeholder="my value" />
+                <div class="grid gap-8">
+                    <flux:input name="label" x-model="label" label="Label" placeholder="my label" />
+                    <flux:input name="value" x-model="value" label="Value" placeholder="value" />
                 </div>
             </div>
             <div class="rounded-lg border border-black/10 p-8 dark:border-white/10">
                 <flux:heading class="mb-6 border-b border-black/10 pb-4 dark:border-white/10" size="xl">2. Preview</flux:heading>
-                <div class="mb-8 grid gap-4">
-                    <div data-barcode-canvas x-ref="barcode-canvas">
-                        <div data-barcode-paper x-ref="barcode-paper">
-                            <div data-barcode x-ref="barcode">
-                                <div data-barcode-label x-ref="label"><span x-text="getLabel()">my label</span></div>
-                                <div data-barcode-code x-ref="code"><span style="font-family: 'Libre Barcode 128';" x-text="getCode()">ÌvalueÈÎ</span></div>
-                                <div data-barcode-value x-ref="value"><span x-text="getValue()">value</span></div>
+                <div class="mb-6 grid gap-4">
+                    <flux:subheading class="text-center !font-normal" size="lg">
+                        Here's your barcode.
+                    </flux:subheading>
+                    <div data-barcode-canvas x-ref="barcodeCanvas">
+                        <div data-barcode-paper>
+                            <div data-barcode>
+                                <div data-barcode-label><span x-text="getLabel()">my label</span></div>
+                                <div data-barcode-code><span style="font-family: 'Libre Barcode 128';" x-text="getCode()">ÌvalueÈÎ</span></div>
+                                <div data-barcode-value><span x-text="getValue()">value</span></div>
                             </div>
                         </div>
                     </div>
-                    {{-- <img class="mx-auto w-20" src="{{ asset('image/barcode.png') }}"width="128" height="128"> --}}
                 </div>
-                <flux:button class="w-full" variant="primary">
+                <flux:button class="w-full cursor-pointer" type="submit" variant="primary">
                     Print
                 </flux:button>
             </div>
             <div class="col-span-2 rounded-lg border border-black/10 p-8 dark:border-white/10">
                 <flux:heading class="mb-6 border-b border-black/10 pb-4 dark:border-white/10" size="xl">3. Share</flux:heading>
-                <flux:subheading>
+                <flux:subheading class="mb-2">
                     You can link to this page with url parameters.
-
-                    You can link to this page with url parameters.
-                    Example: http://savvygoose.com/barcode-generator?value=12345&label=mylabel&print=true
-
-                    Test the example link above.
                 </flux:subheading>
+
+                <div class="grid gap-4">
+
+                    <flux:input
+                        type="url"
+                        x-model="url"
+                        readonly
+                        x-model="url"
+                        copyable
+                        label="Share URL"
+                    />
+                    <flux:checkbox x-model="print" label="Print when open link" />
+                </div>
             </div>
         </div>
 
-    </div>
+    </form>
 </x-layouts.app>
